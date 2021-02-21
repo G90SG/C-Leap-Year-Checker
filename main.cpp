9c7d9e382a587fac8abf62a54568124744a4e783
G90SG <71718345+G90SG@users.noreply.github.com>
2021-02-21 17:33:24 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
+// Calling standard library
 #include <iostream>
 
 int main() {
-  /* declaring the variable */
+// Declaring the 'year' variable as an integer
   int year;
-  /* asking the user for the year they would like to check*/
+// Asking the user for the year they would like to check
   printf ("Enter a year after 1752 to find out if it is a leap year: \n");
-  /* checking input and allocating it to 'year' */
+// Checking input and allocating it to 'year'
   scanf ("%d",& year);
-  /* creating if statement to % 400 , 100 ,4 to determine whether it is or is not a leap year*/
+// Creating IF statement to divide by 400, 100, 4 to determine whether it is or is not a leap year
   if (year%400 == 0)
     printf ("%d is a leap year", year);
   else if (year%100 == 0)
